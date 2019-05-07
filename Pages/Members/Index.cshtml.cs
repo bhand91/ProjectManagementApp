64ac3e95dd3e9af38d5cb969c40162443a0757c0
Braley Hand <braley@amacha.local>
2019-05-07 11:19:32 -0500

    5.7.19

--- Pages/Members/Index.cshtml.cs
+++ Pages/Members/Index.cshtml.cs
@@ -18,11 +18,51 @@ namespace ProjectManagementApp.Pages.Members
             _context = context;
         }
 
-        public IList<Member> Member { get;set; }
+        public PaginatedList<Member> Member { get;set; }
 
-        public async Task OnGetAsync()
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public string LastNameSort {get; set;}
+        public string FirstNameSort {get; set;}
+        public string CurrentFilter {get; set;}
+        public string CurrentSort {get; set;}
+        public async Task OnGetAsync(string sortOrder, string searchString, string currentFilter, int? pageIndex)
         {
-            Member = await _context.Members.ToListAsync();
+            CurrentSort = sortOrder;
+            LastNameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            CurrentFilter = searchString;
+
+            if(searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            CurrentFilter = searchString;
+
+            IQueryable<Member> memberQuery = from m in _context.Members
+                                                select m;
+            if(!string.IsNullOrEmpty(searchString))
+            {
+                memberQuery = memberQuery.Where(m => m.FirstName.ToUpper().Contains(SearchString.ToUpper()) || m.LastName.ToUpper().Contains(SearchString.ToUpper()));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    memberQuery = memberQuery.OrderByDescending(m => m.LastName);
+                    break;
+                default:
+                    memberQuery = memberQuery.OrderBy(m => m.LastName);
+                    break;
+            }
+
+            int pageSize = 10;
+
+            Member = await PaginatedList<Member>.CreateAsync(memberQuery.Include(pm => pm.ProjectMembers).ThenInclude(p => p.Project).AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
