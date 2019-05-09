558378b0581e8b31af16e3708ec2e3eded19b030
bhand91 <43619765+bhand91@users.noreply.github.com>
2019-05-08 20:46:11 -0500

    5.8.19

--- Models/Project.cs
+++ Models/Project.cs
@@ -9,7 +9,6 @@ namespace ProjectManagementApp.Models
         public int ProjectID {get; set;}
 
         [Display(Name = "Project Name")]
-        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Use letters only please")]
         [StringLength(50, ErrorMessage = "Project name cannot be longer than 50 characters.")]
 
         public string ProjectName {get; set;}
